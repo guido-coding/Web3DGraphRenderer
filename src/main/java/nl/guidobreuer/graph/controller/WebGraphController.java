19ca885337ee20a7cc0d43b9b2dd2a8624ad0ef9
guido-coding <>
2026-07-04 15:14:25 +0200

    implemented bucket4j to limit number of request that can be made to APi

--- src/main/java/nl/guidobreuer/graph/controller/WebGraphController.java
+++ src/main/java/nl/guidobreuer/graph/controller/WebGraphController.java
@@ -3,7 +3,7 @@ package nl.guidobreuer.graph.controller;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.util.Map;
-
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
 import equationparser.InvalidEquationException;
 import nl.guidobreuer.graph.exception.InvalidInputException;
 import nl.guidobreuer.graph.model.RenderingSettings;
@@ -19,6 +21,7 @@ import nl.guidobreuer.graph.model.graph3d.Graph3DRenderer;
 import nl.guidobreuer.graph.service.Graph3DRendererBuilder;
 import nl.guidobreuer.graph.util.Util;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 /*
@@ -27,12 +30,26 @@ import java.time.LocalDateTime;
 @RestController
 public class WebGraphController {
 
+	// bucket with capacity 20 tokens and with refilling speed 1 token per each 2 second
+	private Bucket createBucket(String ip) {
+		return Bucket.builder()
+			      .addLimit(limit -> limit.capacity(20).refillGreedy(30, Duration.ofMinutes(1)))
+			      .build();
+	}
 	
-	
+	private final Map<String, Bucket> bucketMap = new ConcurrentHashMap<>();
 
 	@GetMapping("/image")
 	public ResponseEntity<Resource> start(
+			HttpServletRequest request,
 			@RequestParam Map<String, String> options) throws InvalidEquationException, InvalidInputException {
+		
+
+		String ip = extractClientIp(request);
+		Bucket bucket = bucketMap.computeIfAbsent(ip, this::createBucket);
+		if (!bucket.tryConsume(1)) {
+			throw new IllegalStateException("Server timeout. Too many requests. Only 1 request per second allowed.");
+		}
 		
 		long start = System.currentTimeMillis();
 		
@@ -45,12 +62,27 @@ public class WebGraphController {
 		long end = System.currentTimeMillis();
 		long duration = end-start;
 		
-		System.out.println(LocalDateTime.now() + ": --- Graph rendered in " + duration + " ms.");
+		System.out.println(ip + " --- " + LocalDateTime.now() + ": --- Graph rendered in " + duration + " ms.");
 		
 		
 		return Util.imageToByteOutput(image);
 	}
 
+	
+	private String extractClientIp(HttpServletRequest request) {
+	    String header = request.getHeader("X-Forwarded-For");
+	    if (header != null && !header.isBlank()) {
+	        String[] ips = header.split(",");
+	        return ips[0].trim();
+	    }
+
+	    String fallback = request.getHeader("X-Real-IP");
+	    if (fallback != null && !fallback.isBlank()) {
+	        return fallback.trim();
+	    }
+
+	    return request.getRemoteAddr();
+	}
 		
 	
 	@ExceptionHandler(Exception.class)
